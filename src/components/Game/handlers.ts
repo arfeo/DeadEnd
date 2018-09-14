877d3832aa21befa4bc4a02f6180a0b0c87cff0f
Leonid Belikov <leonid@belikov.net>
2018-09-14 11:27:02 +0300

    + steps count

--- src/components/Game/handlers.ts
+++ src/components/Game/handlers.ts
@@ -1,4 +1,5 @@
 import { ballMove } from './animation';
+import { resetPanelInfoValues } from './render';
 
 /**
  * Set up game event handlers
@@ -17,28 +18,44 @@ function keyDownHandler(event: KeyboardEvent) {
     switch (event.key) {
       case 'ArrowUp':
       case 'Up': {
-        ballMove.call(this, 'up').catch(() => {
+        ballMove.call(this, 'up').then(() => {
+          this.stepsCount += 1;
+
+          resetPanelInfoValues.call(this);
+        }).catch(() => {
           console.log('Cannot move here');
         });
         break;
       }
       case 'ArrowRight':
       case 'Right': {
-        ballMove.call(this, 'right').catch(() => {
+        ballMove.call(this, 'right').then(() => {
+          this.stepsCount += 1;
+
+          resetPanelInfoValues.call(this);
+        }).catch(() => {
           console.log('Cannot move here');
         });
         break;
       }
       case 'ArrowDown':
       case 'Down': {
-        ballMove.call(this, 'down').catch(() => {
+        ballMove.call(this, 'down').then(() => {
+          this.stepsCount += 1;
+
+          resetPanelInfoValues.call(this);
+        }).catch(() => {
           console.log('Cannot move here');
         });
         break;
       }
       case 'ArrowLeft':
       case 'Left': {
-        ballMove.call(this, 'left').catch(() => {
+        ballMove.call(this, 'left').then(() => {
+          this.stepsCount += 1;
+
+          resetPanelInfoValues.call(this);
+        }).catch(() => {
           console.log('Cannot move here');
         });
         break;
